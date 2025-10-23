a489d523df533718b1f885f7025e9fa3c7ae707e
shallagehmasood <shallagehmasood@gmail.com>
2025-10-23 16:45:48 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,20 @@
-import 'dart:convert';
-import 'dart:typed_data';
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
+import 'screens/settings_page.dart';
+import 'screens/outbox_page.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'First Hidden App',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const ImageStreamPage(),
-    );
-  }
-}
-
-class ImageStreamPage extends StatefulWidget {
-  const ImageStreamPage({super.key});
-
-  @override
-  State<ImageStreamPage> createState() => _ImageStreamPageState();
-}
-
-class _ImageStreamPageState extends State<ImageStreamPage> {
-  final String wsUrl = "ws://178.63.171.244:8000/ws"; // آدرس سرور WebSocket
-  late WebSocketChannel channel;
-
-  Uint8List? imageData;
-  String? filename;
-
-  @override
-  void initState() {
-    super.initState();
-    channel = WebSocketChannel.connect(Uri.parse(wsUrl));
-
-    channel.stream.listen(
-      (message) {
-        final decoded = jsonDecode(message);
-        setState(() {
-          filename = decoded['filename'];
-          imageData = base64Decode(decoded['data']);
-        });
-      },
-      onError: (error) {
-        print("❌ WebSocket error: $error");
-      },
-      onDone: () {
-        print("⚠️ WebSocket closed!");
-      },
-    );
-  }
-
-  @override
-  void dispose() {
-    channel.sink.close();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('First Hidden Images')),
-      body: Center(
-        child: imageData != null
-            ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(filename ?? "", style: const TextStyle(fontSize: 16)),
-                  const SizedBox(height: 10),
-                  Image.memory(imageData!),
-                ],
-              )
-            : const Text(
-                'در انتظار تصویر جدید...',
-                style: TextStyle(fontSize: 18),
-              ),
+      title: 'Signal Bot',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: SettingsPage(userId: 12345), // نمایش صفحه تنظیمات برای کاربر 12345
     );
   }
 }
